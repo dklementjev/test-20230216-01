34ea7beb8c260da6e2bda53e903c165cb3e3ccfa
Denis Klementjev <dklementjev@yandex.ru>
2023-02-16 18:59:20 +0300

    Form data validation is prototyped

--- src/Controller/RootController.php
+++ src/Controller/RootController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use App\Form;
 use App\FormData;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RootController extends AbstractController
 {
     #[Route('/', name: 'app_root')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $formData = new FormData\ProductPurchase();
         $form = $this->createForm(
@@ -19,10 +20,24 @@ class RootController extends AbstractController
             $formData, 
             ['method'=>"POST"]
         );
+        if ($request->isMethod("POST")) {
+            $form->handleRequest($request);
+            if ($form->isValid()){
+                return $this->redirectToRoute(
+                    "app_purchase", 
+                    [
+                        'product' => $formData->getProduct()->getId(),
+                        'tax_id' => $formData->getTaxID()
+                    ]
+                );
+            }
+        }
 
-        return $this->render('root/index.html.twig', [
-            'controller_name' => 'RootController',
-            'form' => $form
-        ]);
+        return $this->render(
+            'root/index.html.twig', 
+            [
+                'form' => $form
+            ]
+        );
     }
 }
