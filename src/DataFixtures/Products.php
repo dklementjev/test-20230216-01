f55bb46dde441d2fdca81d374a1cc3244bca7f47
Denis Klementjev <dklementjev@yandex.ru>
2023-02-16 17:39:24 +0300

    Products data fixture is added

---
+++ src/DataFixtures/Products.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use App\Entity;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class Products extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $data = [
+            ['name' => "Headphones", 'price' => 100],
+            ['name' => "Phone cover", 'price' => 20]
+        ];
+
+        foreach ($data as $dataRow) {
+            $product = new Entity\Product();
+            $product->setName($dataRow['name']);
+            $product->setPrice($dataRow['price']);
+            $manager->persist($product);
+        }
+        
+        $manager->flush();
+    }
+}
